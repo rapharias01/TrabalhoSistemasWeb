f3c0c81f91b1a7699c3f3ba96824f6ac84c80f0d
Raphael Farias <raphaddf01@gmail.com>
2024-10-19 13:40:53 -0300

    TrabalhoWebCrawler

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # TrabalhoSistemasWeb
+# Descrição
+Este script em Python realiza a extração de dados sobre carros usados disponíveis para compra no site Mobiauto. Ele coleta informações como o nome do carro, motor, preço e quilometragem, e armazena esses dados em um arquivo CSV chamado CarrosCompra.csv.
+Após a execução, um arquivo chamado CarrosCompra.csv será criado no mesmo diretório, contendo os dados dos carros extraídos.
+
+# Estrutura do Arquivo CSV
+O arquivo CSV gerado terá as seguintes colunas:
+
+## Carro: Nome do carro
+## Motor: Especificação do motor
+## Preço: Preço do carro
+## Km: Quilometragem do carro
+Observações
+O script verifica se o arquivo CarrosCompra.csv já existe e o remove antes de criar um novo, garantindo que os dados estejam sempre atualizados.
+Caso ocorra um erro de conexão, uma mensagem apropriada será exibida.
---
+++ main.py
@@ -0,0 +1,50 @@
+import requests
+import bs4
+import os
+
+url = "https://www.mobiauto.com.br/comprar/carros-usados/es-vitoria"
+
+def getCarros():
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        if os.path.exists("CarrosCompra.csv"):
+            os.remove("CarrosCompra.csv")
+
+        print("OK! Processando dados...")
+
+        write = "Carro;Motor;Preço;Km"
+
+        soup = bs4.BeautifulSoup(response.text, 'html.parser')
+            
+        divs = soup.find_all('div', class_='css-1qtpr08')
+            
+        for div in divs:
+            Carro_div = div.find('h2', class_='css-1exrr02')
+            carro = Carro_div.text.strip() if Carro_div else "Título não encontrado"
+
+            Motor_div = div.find('h3', class_='css-8p76iz')
+            motor = Motor_div.text.strip() if Motor_div else "Descrição não encontrada"
+
+            preco_div = div.find('p', class_='css-1u3gc91')
+            preco = preco_div.text.strip() if preco_div else "Preço não encontrado"
+
+            Km_div = div.find('div', class_='css-e20x35')
+            km = Km_div.text.strip() if Km_div else "Km não encontrada"
+
+            write += "\n"+carro+";"+motor+";"+preco+";"+km+";"
+
+            print(f"Carro: {carro}")
+            print(f"Motor: {motor}")
+            print(f"Preço: {preco}")
+            print(f"Km: {km}")
+            print('-' * 40)
+
+
+        f = open("CarrosCompra.csv", "a")
+        f.write(write)
+        f.close()    
+    else:
+        print("Erro de conexão\n")
+getCarros()
